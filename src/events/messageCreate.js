020f641c3b609937b7c51317b10d052d3a47c65e
Avery <avery.doucet03@gmail.com>
2022-10-31 18:24:34 -0300

    fix

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -4,16 +4,16 @@ module.exports = {
   name: 'messageCreate',
   execute(message) {
     // Regex match domain is https://twitter.com/*
-    const regex = /https:\/\/twitter.com\/\w+/g;
+    
+    if (message.content.includes('https://twitter.com/')) {
+      // replace it
+      const replacedValue = message.content.replace(
+        'https://twitter.com/',
+        'https://vxtwitter.com/'
+      );
 
-    if (message.content.match(regex)) {
-        const url = message.content.match(regex)[0];
-        const domain = url.split('/')[2];
-
-        // Check if the domain is a twitter link and not a vxtwitter link
-        if (domain === 'twitter.com' && !url.includes('vxtwitter')) {
-            message.channel.send(`https://vxtwitter.com/${url.split('/')[3]}`);
-        }
+        // send the message
+        message.channel.send(replacedValue);
     }
 },
 };
